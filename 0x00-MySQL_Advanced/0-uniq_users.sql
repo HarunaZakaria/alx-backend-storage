adc2e6caf5c4000e397036f0bc3015937277d030
harunaZakaria <Harunzy55@gmail.com>
2023-04-10 08:11:42 +0000

    add a solution to task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+--Write a SQL script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+this is a solution  to 0x00-MySQL_Advanced task
