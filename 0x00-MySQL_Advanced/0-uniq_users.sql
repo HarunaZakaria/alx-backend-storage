db8cc72927c4751687177bafcd0d20cfaf724d2a
harunaZakaria <Harunzy55@gmail.com>
2023-04-11 09:19:29 +0000

    add

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 --Write a SQL script that creates a table users with the following
 --name, email, and id
 CREATE TABLE IF NOT EXISTS users (
-  id int PRIMARY KEY AUTOINCREMENT NOT NULL,
-  email varchar(255) UNIQUE NOT NULL,
-  name varchar(255) 
-)
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(255),
+  PRIMARY KEY(id) 
+);
