31c44168456a26e91f9b097d880ecd275adf2924
harunazakaria <Harunzy55@gmail.com>
2023-04-15 07:38:11 +0000

    add

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,6 +5,8 @@ update  school names by topic
 
 import pymongo
 
+
 def update_topics(mongo_collection, name, topics):
- return mongo_collection.update_many({"name": name},
+    """changes all topics of a school document"""
+    return mongo_collection.update_many({"name": name},
                                         {"$set": {"topics": topics}})
