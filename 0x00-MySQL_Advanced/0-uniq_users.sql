b538f928907f83658486f7f23cbef59389094b77
harunaZakaria <Harunzy55@gmail.com>
2023-04-22 11:38:31 +0000

    add first 3 task

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
---Write a SQL script that creates a table users with the following
---name, email, and id
+-- A SQL script that creates a table (called users) with following fields
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  name VARCHAR(255),
-  PRIMARY KEY(id) 
-);
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
---Write a SQL script that creates a table users following these requirements:
---id, name, email, and country.
-CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  name VARCHAR(255),
-  country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
-  PRIMARY KEY(id) 
+-- A SQL script that creates a table (called users) following these requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
